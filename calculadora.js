617d4497fd918823f8922a9b777bd9adaeb5b6ce
fvcbeach <98333450+fvcbeach@users.noreply.github.com>
2022-02-09 11:18:57 -0300

    Actividad Calculadora

---
+++ calculadora.js
@@ -0,0 +1,56 @@
+var operandoa;
+var operandob;
+var operandon;
+function init();{
+    var Reset = document.getElementById("Reset");
+    var Borrar = document.getElementById("Borrar");
+    var Suma = document.getElementById("Suma");
+    var Uno = document.getElementById("Uno");
+    var Dos = document.getElementById("Dos");
+    var Tres = document.getElementById("Tres");
+    var Resta = document.getElementById("Resta");
+    var Cuatro = document.getElementById("Cuatro");
+    var Cinco = document.getElementById("Cinco");
+    var Seis = document.getElementById("Seis");
+    var Division = document.getElementById("Division");
+    var Siete = document.getElementById("Siete");
+    var Ocho = document.getElementById("Ocho");
+    var Nueve = document.getElementById("Nueve");
+    var Multiplicacion = document.getElementById("Multiplicacion");
+    var Cero = document.getElementById("Cero");
+    var Resultado = document.getElementById("Resultado");
+
+    Uno.onclick = function(e){
+        resultado.textContent = resultado.textContent + "1";
+    }
+    Dos.onclick = function(e){
+        resultado.textContent = resultado.textContent + "2";
+    }
+    Tres.onclick = function(e){
+        resultado.textContent = resultado.textContent + "3";
+    }
+    Cuatro.onclick = function(e){
+        resultado.textContent = resultado.textContent + "4";
+    }
+    Cinco.onclick = function(e){
+        resultado.textContent = resultado.textContent + "5";
+    }
+    Seis.onclick = function(e){
+        resultado.textContent = resultado.textContent + "6";
+    }
+    Siete.onclick = function(e){
+        resultado.textContent = resultado.textContent + "7";
+    }
+    Ocho.onclick = function(e){
+        resultado.textContent = resultado.textContent + "8";
+    }
+    Nueve.onclick = function(e){
+        resultado.textContent = resultado.textContent + "9";
+    }
+    Cero.onclick = function(e){
+        resultado.textContent = resultado.textContent + "0";
+    }
+    Reset.onclick = function(e){
+        Reset;
+    Suma.onclick= 
+}
